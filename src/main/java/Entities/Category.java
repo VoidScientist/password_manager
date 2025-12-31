4d01a3802ca8e4c00f7b56869823f52b2863cee4
VoidScientist <louthosi59@gmail.com>
2025-12-31 12:31:32 +0100

    Implémentation de la classe Category sans relations (tmp)

---
+++ src/main/java/Entities/Category.java
@@ -0,0 +1,72 @@
+package Entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Category {
+
+    @Id
+    @Column(name="ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="NAME")
+    private String name;
+
+    @Column(name="DESC")
+    private String desc;
+
+    public Category(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public Category() {
+        this.name = "";
+        this.desc = "";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return getId() == category.getId() && Objects.equals(getName(), category.getName()) && Objects.equals(getDesc(), category.getDesc());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDesc());
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
+
+}
