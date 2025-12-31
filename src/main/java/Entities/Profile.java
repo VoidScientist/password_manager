df86f5ddc22bb1d2ee9855282c1958e8dbbfa7d0
VoidScientist <louthosi59@gmail.com>
2025-12-31 13:05:50 +0100

    Implémentation de la classe Profile sans relations (tmp)

---
+++ src/main/java/Entities/Profile.java
@@ -0,0 +1,33 @@
+package Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="SERVICE")
+    private String service;
+
+    @Column(name="USERNAME")
+    private String username;
+
+    @Column(name="PASSWORD")
+    private String encrypted_password;
+
+    @Column(name="URL")
+    private String url;
+
+    public Profile() {}
+
+    public Profile(String service, String username, String encrypted_password, String url) {
+        this.service = service;
+        this.username = username;
+        this.encrypted_password = encrypted_password;
+        this.url = url;
+    }
+
+}
