72ea835a5ffac38e0925b0a88ac87b5f36624992
VoidScientist <louthosi59@gmail.com>
2025-12-31 20:44:07 +0100

    Finalisation de Profile

--- src/main/java/Entities/Profile.java
+++ src/main/java/Entities/Profile.java
@@ -2,6 +2,13 @@ package Entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
+/**
+ * La classe Profile est l'entité JPA contenant les informations liées aux comptes enregistrés.
+ *
+ * C'est la propriétaire des relations avec les autres entités dans la base de donnée.
+ */
 @Entity
 public class Profile {
 
@@ -36,6 +43,87 @@ public class Profile {
         this.username = username;
         this.encrypted_password = encrypted_password;
         this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEncrypted_password() {
+        return encrypted_password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public UserProfile getOwner() {
+        return owner;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEncrypted_password(String encrypted_password) {
+        this.encrypted_password = encrypted_password;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public void setOwner(UserProfile owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Profile profile = (Profile) o;
+        return Objects.equals(getService(), profile.getService()) && Objects.equals(getUsername(), profile.getUsername()) && Objects.equals(getEncrypted_password(), profile.getEncrypted_password()) && Objects.equals(getUrl(), profile.getUrl()) && Objects.equals(getOwner(), profile.getOwner()) && Objects.equals(getCategory(), profile.getCategory());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getService(), getUsername(), getEncrypted_password(), getUrl(), getOwner(), getCategory());
+    }
+
+    @Override
+    public String toString() {
+
+        String ownerRep = owner != null ? owner.getUuid() : null;
+        String categoryRep = category != null ? category.getName() : null;
+
+        return "Profile{" +
+                "id=" + id +
+                ", service='" + service + '\'' +
+                ", username='" + username + '\'' +
+                ", encrypted_password='" + encrypted_password + '\'' +
+                ", url='" + url + '\'' +
+                ", owner=" + ownerRep +
+                ", category=" + categoryRep +
+                '}';
     }
 
 }
