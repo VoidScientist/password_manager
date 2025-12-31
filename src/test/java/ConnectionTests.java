fae90f1dfd08e7840f50e63642746c1e849807b8
VoidScientist <louthosi59@gmail.com>
2025-12-31 16:11:57 +0100

    renommage d'un fichier test

---
+++ src/test/java/ConnectionTests.java
@@ -0,0 +1,53 @@
+import Entities.UserProfile;
+import Utilities.Config;
+import Repositories.UserProfileRepository;
+import Utilities.Security.PasswordHasher;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+public class ConnectionTests {
+
+    public static final String USERNAME = "TestUser987";
+    public static final String PASSWORD = "test8/+";
+
+    static UserProfileRepository userRep;
+
+    @BeforeAll
+    public static void init() throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        userRep = new UserProfileRepository(Config.getPersistenceUnit());
+
+        String hash = PasswordHasher.hashPassword(PASSWORD.toCharArray());
+
+        UserProfile user = new UserProfile(USERNAME, hash);
+
+        userRep.save(user);
+
+    }
+
+    @Test
+    public void rightPassword() throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        UserProfile user = userRep.findByUsername(USERNAME);
+
+        System.out.println(user.toString());
+
+        assert user.connect(PASSWORD);
+
+    }
+
+    @Test
+    public void wrongPassword() throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        UserProfile user = userRep.findByUsername(USERNAME);
+
+        System.out.println(user.toString());
+
+        assert !user.connect("ahahamhackeridon'tknowyourpassword12431554");
+
+    }
+
+}
