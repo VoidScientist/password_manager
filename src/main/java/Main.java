7d4446b70ff975df03ac2438bd048afc0f72d8f0
VoidScientist <louthosi59@gmail.com>
2026-01-15 00:04:50 +0100

    Ajout initialisation des services

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
+import Managers.ServiceManager;
 import UI.MainFrame;
+import Utilities.Config;
+
 
 import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        ServiceManager.init(Config.getPersistenceUnit());
+
         SwingUtilities.invokeLater(() -> {
             try {
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
