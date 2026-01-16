afd3de2026dc3e8b494e54c448dffd4f55d6bf41
VoidScientist <louthosi59@gmail.com>
2026-01-16 15:16:42 +0100

    ajout documentation Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,10 @@ import Utilities.Config;
 
 import javax.swing.*;
 
+/**
+ * Point d'entrée du programme. 
+ * Crée l'UI et initialise le ServiceManager.
+ */
 public class Main {
 
     public static void main(String[] args) {
