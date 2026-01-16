0964e24c15a954fe03daaf2271ae3a99801c8e0c
ISO-5CD7081M9J\Administrateur <martelmathieu0404@gmail.com>
2026-01-16 15:44:14 +0100

    Ajouts de commentaires et JavaDoc pour les classes PasswordGenerator, PasswordStats, PasswordStrength et WeakPasswordDictionary.

--- src/main/java/Utilities/Security/Password/PasswordGenerator.java
+++ src/main/java/Utilities/Security/Password/PasswordGenerator.java
@@ -2,10 +2,30 @@ package Utilities.Security.Password;
 
 import java.security.SecureRandom;
 
+/**
+ * Classe utilitaire pour générer des mots de passe aléatoires.
+ * Permet de personnaliser les caractères utilisés (majuscules, chiffres, symboles)
+ * et d'exclure les caractères ambigus (l, I, O, 0, 1).
+ *
+ * @author ARCELON Louis, MARTEL Mathieu
+ * @version v0.1
+ */
+
 public class PasswordGenerator {
 
     private static final SecureRandom random = new SecureRandom();
 
+    /**
+     * Génère un mot de passe selon les critères spécifiés.
+     *
+     * @param length Longueur du mot de passe à générer (minimum 1)
+     * @param useUppercase Si true, inclut des lettres majuscules
+     * @param useDigits Si true, inclut des chiffres
+     * @param useSymbols Si true, inclut des symboles
+     * @param allowAmbiguous Si false, exclut les caractères ambigus (l, I, O, 0, 1)
+     * @return Le mot de passe généré
+     * @throws IllegalArgumentException Si length < 1 ou si aucun caractère n'est disponible
+     */
     public static String generate(int length, boolean useUppercase, boolean useDigits,
                                   boolean useSymbols, boolean allowAmbiguous) {
         if (length < 1) {
@@ -18,6 +38,7 @@ public class PasswordGenerator {
         // Minuscules (toujours incluses)
         String lowercase = "abcdefghijklmnopqrstuvwxyz";
         if (!allowAmbiguous) {
+            // Exclure 'l' et 'o'
             lowercase = lowercase.replace("l", "").replace("o", "");
         }
         pool.append(lowercase);
@@ -26,6 +47,7 @@ public class PasswordGenerator {
         if (useUppercase) {
             String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             if (!allowAmbiguous) {
+                // Exclure 'I' et 'O'
                 uppercase = uppercase.replace("I", "").replace("O", "");
             }
             pool.append(uppercase);
@@ -35,6 +57,7 @@ public class PasswordGenerator {
         if (useDigits) {
             String digits = "0123456789";
             if (!allowAmbiguous) {
+                // Exclure '1' et '0'
                 digits = digits.replace("0", "").replace("1", "");
             }
             pool.append(digits);
--- src/main/java/Utilities/Security/Password/PasswordStats.java
+++ src/main/java/Utilities/Security/Password/PasswordStats.java
@@ -1,13 +1,27 @@
 package Utilities.Security.Password;
 
+/**
+ * Classe utilitaire pour analyser les caractères d'un mot de passe.
+ * Calcule le nombre de caractères de chaque type (minuscules, majuscules, chiffres, symboles).
+ *
+ * @author ARCELON Louis, MARTEL Mathieu
+ * @version v0.1
+ */
+
 public class PasswordStats {
 
-    private int length;
-    private int lowercase;
-    private int uppercase;
-    private int digits;
-    private int symbols;
+    private int length;     // Longueur totale
+    private int lowercase;  // Nombre de minuscules
+    private int uppercase;  // Nombre de maujuscules
+    private int digits;     // Nombre de chiffres
+    private int symbols;    // Nombre de symboles
 
+    /**
+     * Analyse le mot de passe en paramètre.
+     * Compte le nombre de minuscules, majuscules, chiffres et symboles.
+     *
+     * @param password Le mot de passe à analyser
+     */
     public PasswordStats(String password) {
         this.length = password.length();
         this.lowercase = 0;
@@ -15,6 +29,7 @@ public class PasswordStats {
         this.digits = 0;
         this.symbols = 0;
 
+        // Parcourir chaque caractère et incrémenter le compteur correspondant
         for (char c : password.toCharArray()) {
             if (Character.isLowerCase(c)) lowercase++;
             else if (Character.isUpperCase(c)) uppercase++;
